5e7e864362827532a57f7a28202e410cf83623d0
Georgedmitri <aksanazfa4@gmail.com>
2025-05-15 14:49:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Mauricitarumboys🤓")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Kenapa rambut mauri kayak jamur busuk"
 )
